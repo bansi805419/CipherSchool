4c9924d343097b03a37db71f863383243c36afdd
Bansi Dhar Jha <160783424+bansi805419@users.noreply.github.com>
2024-07-15 05:57:06 +0530

    methodoverriding.java

--- Lecture 9 Pt. 2 ChipherSchools/methodoverriding.java
+++ Lecture 9 Pt. 2 ChipherSchools/methodoverriding.java
@@ -1,1 +1,19 @@
 
+Package PrimitiveDT;
+class parent{
+  void fun()
+  {
+    System.out.println("Hi I'm A");
+  }
+}
+class child extends parent{
+  void fun(){
+    System.out.println("Hi I'm B");
+  }
+}
+public class methodoverriding{
+  public static void main(String[] args){
+    child c= new child();
+    c.fun();
+  }
+}
