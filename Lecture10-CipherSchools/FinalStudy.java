696615fbb7cf549be1653a1c236911d0827ccacf
Bansi Dhar Jha <160783424+bansi805419@users.noreply.github.com>
2024-07-15 11:29:17 +0530

    Create FinalStudy.java

---
+++ Lecture10-CipherSchools/FinalStudy.java
@@ -0,0 +1,25 @@
+package keywords;
+class A{
+  final int noOfAlphabets=26;
+  final int noOfDigits;
+  A(){
+    noOfDigits=10;
+  }
+  final void fun(){
+    System.out.println("Hi, I'm a function in class A")
+  }
+}
+class B extends A{
+  // void fun()
+  // {
+  //   System.out.println("I'm function in aN overriding function in A");
+  // }
+}
+// class c extends B{
+  
+// }
+public class FinalStudy{
+  public static void main(String[] args){
+    
+  }
+}
