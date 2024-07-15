b7b38cd7d61639c81e7371ba76eb8c1a31eba96e
Bansi Dhar Jha <160783424+bansi805419@users.noreply.github.com>
2024-07-15 11:36:14 +0530

    Create B.java

---
+++ Lecture13-CipherSchools/Accessmodifiers1/B.java
@@ -0,0 +1,9 @@
+package accessmodifiers1;
+public class B{
+  public static void main(String[] args){
+    A obj= new A();
+    System.out.prinln(obj.x);
+    System.out.println(obj.y);
+    System.out.println(obj.z);
+  }
+}
