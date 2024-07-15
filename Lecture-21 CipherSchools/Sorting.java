846baa56264c7bf995f42e96de14d7a4bf5b12a6
Bansi Dhar Jha <160783424+bansi805419@users.noreply.github.com>
2024-07-15 13:35:43 +0530

    Create Sorting.java

---
+++ Lecture-21 CipherSchools/Sorting.java
@@ -0,0 +1,23 @@
+class Compute
+  {
+    Static pait getMinMax(long arr[],long n)
+    {
+      long min =arr[0];
+      for(int i=0;i<n;i++)
+        {
+          if(arr[i]<min)
+          {
+            min=arr[i];
+          }
+        }
+      long max=arr[0];
+      for(int i=0;i<n;i++)
+        {
+          if(int i=0;i<n;i++)
+            {
+            max=arr[i];
+          }
+        }
+      return new pair(min.max);
+    }
+  }
