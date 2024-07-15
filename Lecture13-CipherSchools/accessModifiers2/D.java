e2851d993cbbc31e6423f522b13ce4d932bf00ba
Bansi Dhar Jha <160783424+bansi805419@users.noreply.github.com>
2024-07-15 11:38:05 +0530

    Create D.java

---
+++ Lecture13-CipherSchools/accessModifiers2/D.java
@@ -0,0 +1,12 @@
+package accessModifiers2;
+import Accessmodifiers1.A;
+public class D{
+  public static void main(String[] args){
+    A obj= new A();
+    //System.out.println(obj.x);
+    // System.out.println(obj.y);
+    // System.out.println(obj.z);
+    // System.out.prinln(obj.w);
+  } 
+}
+  
